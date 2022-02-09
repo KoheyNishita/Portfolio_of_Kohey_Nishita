4273f9e3213d64a494d6b7cf95d6dfdc6b4a0cc8
KoheyNishita <5024kohy.nishi@gmail.com>
2022-02-09 20:55:16 +0900

    css, js add, pic changed

--- views/js/back_img.js
+++ views/js/back_img.js
@@ -3,14 +3,13 @@
 /////////////////////////////
 
 // ページの上下スクロールに応じて背景画像も上下に動く
-// ページ高さが400vh動いている間に画像が200vh動く
 $(window).on('scroll', function () {
 
     var scrollTop = $(window).scrollTop();
-    var bgPosition = scrollTop / 2; //スクロール後のポジションを指定（値を大きくすると移動距離が小さくなる）
+    var bgPosition = scrollTop / 5; //スクロール後のポジションを指定（値を大きくすると移動距離が小さくなる）
 
     if (bgPosition) {
-        $('#back-js').css('background-position', 'center top -' + bgPosition + 'px');
+        $('#back-js').css('background-position', 'left top -' + bgPosition + 'px');
     }
 });
 
