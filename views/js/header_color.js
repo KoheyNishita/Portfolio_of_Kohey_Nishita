03a0816c31ca098955da273081084309adb9e8e2
KoheyNishita <5024kohy.nishi@gmail.com>
2022-02-12 22:02:56 +0900

    css,html,js changed / js updating now

--- views/js/header_color.js
+++ views/js/header_color.js
@@ -2,22 +2,15 @@
 // Header - Color Change
 /////////////////////////////
 
-// jQuery
-// 1ページ分スクロールしたらヘッダーの背景色が透明・文字色が白になる
-
-// if ('100vh' < jQuery(this).scrollTop()) {
-//     jQuery('.header').addClass('header-scrolled');
-// } else {
-//     jQuery('.header').removeClass('header-scrolled');
-// }
-
 $(function () {
-    $(window).on("scroll", function () {
-        const sliderHeight = $(".page profile").height();
-        if (sliderHeight - 30 < $(this).scrollTop()) {
-            $(".header").addClass("headerScroll");
+    var $offset1 = $('#page1').offset();
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > $offset1.top - 64) {
+            $('.header').addClass('headerFadeOut');
+            $('.header').addClass('headerColorChanged');
         } else {
-            $(".header").removeClass("headerScroll");
+            $('.header').removeClass('headerColorChanged');
         }
     });
 });
