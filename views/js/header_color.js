cea9603a8c39c60457b3f003a3f9920e7a5a2915
KoheyNishita <5024kohy.nishi@gmail.com>
2022-02-13 20:58:07 +0900

    css,html updated for JS

--- views/js/header_color.js
+++ views/js/header_color.js
@@ -7,10 +7,14 @@ $(function () {
 
     $(window).scroll(function () {
         if ($(window).scrollTop() > $offset1.top - 64) {
-            $('.header').addClass('headerFadeOut');
             $('.header').addClass('headerColorChanged');
+            $('.header-list dt').addClass('headerTextChanged');
+            $('.header-list dd').addClass('headerTextChanged');
+
         } else {
             $('.header').removeClass('headerColorChanged');
+            $('.header-list dt').removeClass('headerTextChanged');
+            $('.header-list dd').removeClass('headerTextChanged');
         }
     });
 });
