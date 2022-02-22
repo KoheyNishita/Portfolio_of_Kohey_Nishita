41e1c2bab915474a282a89e719edf302bae3b250
KoheyNishita <5024kohy.nishi@gmail.com>
2022-02-22 12:11:14 +0900

    css updated: js animation

--- views/js/header_line.js
+++ views/js/header_line.js
@@ -17,24 +17,29 @@ $(function () {
             $('#page1').addClass('fixedLine');
         } else {
             $('#page1').removeClass('fixedLine');
+            $('#page1').css({'position':'fixed'});
+
         }
 
         if ($(window).scrollTop() > $offset2.top - 64) {
             $('#page2').addClass('fixedLine');
         } else {
             $('#page2').removeClass('fixedLine');
+            $('#page2').css({'position':'fixed'});
         }
 
         if ($(window).scrollTop() > $offset3.top - 64) {
             $('#page3').addClass('fixedLine');
         } else {
             $('#page3').removeClass('fixedLine');
+            $('#page3').css({'position':'fixed'});
         }
 
         if ($(window).scrollTop() > $offset4.top - 64) {
             $('#page4').addClass('fixedLine');
         } else {
             $('#page4').removeClass('fixedLine');
+            $('#page4').css({'position':'fixed'});
         }
     });
 });
